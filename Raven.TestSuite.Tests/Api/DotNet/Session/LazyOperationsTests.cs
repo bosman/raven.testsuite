d835308aa1dfd29546186f27b56eaeb382f1210c
Daniel Kardach <dkardach@ais.pl>
2014-03-28 09:58:56 +0100

    Lazy loading tests.

--- Raven.TestSuite.Tests/Api/DotNet/Session/LazyOperationsTests.cs
+++ Raven.TestSuite.Tests/Api/DotNet/Session/LazyOperationsTests.cs
@@ -12,8 +12,10 @@
     [RequiresFreshNorthwindDatabaseAttribute]
     public class LazyOperationsTests : BaseDotNetApiTestGroup
     {
-        const string order1Id = "orders/1";
-        const string order2Id = "orders/2";
+        const string order1IdString = "orders/1";
+        const int order1Id = 1;
+        const string order2IdString = "orders/2";
+        const int order2Id = 2;
 
         public LazyOperationsTests(IRavenClientWrapper wrapper)
             : base(wrapper)
@@ -29,17 +31,36 @@
                 {
                     using (var session = store.OpenSession())
                     {
-                        Lazy<Order> lazyOrder = session.Advanced.Lazily.Load<Order>(order1Id);
+                        Lazy<Order> lazyOrder = session.Advanced.Lazily.Load<Order>(order1IdString);
                         Assert.False(lazyOrder.IsValueCreated);
                         var order = lazyOrder.Value;
-                        Assert.Equal(order1Id, order.Id);
+                        Assert.Equal(order1IdString, order.Id);
 
-                        Lazy<Order[]> lazyOrders = session.Advanced.Lazily.Load<Order>(new String[] { order1Id, order2Id });
+                        lazyOrder = session.Advanced.Lazily.Load<Order>(order1Id);
+                        Assert.False(lazyOrder.IsValueCreated);
+                        order = lazyOrder.Value;
+                        Assert.Equal(order1IdString, order.Id);
+
+                        Lazy<Order[]> lazyOrders = session.Advanced.Lazily.Load<Order>(new String[] { order1IdString, order2IdString });
                         Assert.False(lazyOrders.IsValueCreated);
                         Order[] orders = lazyOrders.Value;
                         Assert.Equal(2, orders.Length);
-                        Assert.Equal(order1Id, orders[0].Id);
-                        Assert.Equal(order2Id, orders[1].Id);
+                        Assert.Equal(order1IdString, orders[0].Id);
+                        Assert.Equal(order2IdString, orders[1].Id);
+
+                        lazyOrders = session.Advanced.Lazily.Load<Order>(order1IdString, order2IdString);
+                        Assert.False(lazyOrders.IsValueCreated);
+                        orders = lazyOrders.Value;
+                        Assert.Equal(2, orders.Length);
+                        Assert.Equal(order1IdString, orders[0].Id);
+                        Assert.Equal(order2IdString, orders[1].Id);
+
+                        lazyOrders = session.Advanced.Lazily.Load<Order>(order1Id, order2Id);
+                        Assert.False(lazyOrders.IsValueCreated);
+                        orders = lazyOrders.Value;
+                        Assert.Equal(2, orders.Length);
+                        Assert.Equal(order1IdString, orders[0].Id);
+                        Assert.Equal(order2IdString, orders[1].Id);
                     }
                 }
             });
@@ -57,16 +78,16 @@
                         Order order1 = null;
                         Order order2 = null;
 
-                        session.Advanced.Lazily.Load<Order>(order1Id, x => order1 = x);
-                        session.Advanced.Lazily.Load<Order>(order2Id, x => order2 = x);
+                        session.Advanced.Lazily.Load<Order>(order1IdString, x => order1 = x);
+                        session.Advanced.Lazily.Load<Order>(order2IdString, x => order2 = x);
                         Assert.Null(order1);
                         Assert.Null(order2);
 
                         session.Advanced.Eagerly.ExecuteAllPendingLazyOperations();
                         Assert.NotNull(order1);
                         Assert.NotNull(order2);
-                        Assert.Equal(order1Id, order1.Id);
-                        Assert.Equal(order2Id, order2.Id);
+                        Assert.Equal(order1IdString, order1.Id);
+                        Assert.Equal(order2IdString, order2.Id);
                     }
                 }
             });
